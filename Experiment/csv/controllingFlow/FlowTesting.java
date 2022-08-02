35e460b2c901870eafa8158e55f67d0471e3feef
Biswajit-10 <https://github.com/Biswajit-10/A.git>
2022-08-03 01:16:09 +0530

    Successful

---
+++ Experiment/csv/controllingFlow/FlowTesting.java
@@ -0,0 +1,119 @@
+package csv.controllingFlow;
+
+import java.util.Scanner;
+
+import org.testng.Assert;
+
+public class FlowTesting {
+
+	public static void main(String[] args) {
+		try {
+			System.out.println("argument in main is "+args[0]);
+			}catch (Exception e) {
+				args=new String[1];
+				args[0]="N";
+			}
+		System.out.println("argument in main is "+args[0]);
+
+		for (int i = 0; i < 10; i++) {
+
+			try {
+
+				String RunStatus = "Yes";
+		/*		if Run Status in is Yes then Run else not Run*/		
+				if (RunStatus.equalsIgnoreCase("Yes")) {
+					
+					System.out.println(1/0);
+					// do switch case
+					// All the functions
+					// All the actions
+					
+				}else {
+					
+					/*do nothing*/
+					
+				}
+			} 
+			catch (Exception e) {
+
+				/* printing error message */
+				System.err.println(e.getMessage());
+				System.out.println("\n\n\n");
+				System.err.println("We are getting error at line no: " + i);
+
+				/*
+				 * if we don't want to make porgram stop pass it in main argument Y else fail
+				 * the script
+				 */
+
+				if (args[0].equalsIgnoreCase("Y")) {
+					System.out.println("waiting for your response");
+					String resume = decision();
+
+					if (resume.equalsIgnoreCase("Y")) {
+						i--; // we will try that step once more if input is yes
+					}
+
+				} else {
+					Assert.fail();
+				}
+				
+			}
+
+		}
+
+	}
+
+	/* To Do */
+	/* When you are ready after editing just type y and click enter */
+	/* make it such a way that it can only take input as y or n */
+	public static String input() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("type Y or N");
+		String input = scanner.next();
+
+		return input;
+	}
+
+	public static String decision() {
+		Scanner scanner = new Scanner(System.in);
+		String decision=null;
+		boolean yn = true;
+		while (yn) {
+
+			System.out.println("please enter Y or N");
+			decision = scanner.next();
+
+			System.out.println("you entered the decision: " + decision+"\n");
+
+			switch (decision) {
+			case "Y":
+				yn = false;
+				break;
+			case "y":
+				yn = false;
+				break;
+			case "N":
+				yn = false;
+				break;
+			case "n":
+				yn = false;
+				break;
+			default:
+				System.out.println("please enter a valid Input ");
+			}
+		}
+		return decision;
+	}
+}
+
+/*
+
+1.If you want to edit then pass Y in argument
+
+	if you pass nothing script fails a[0] array index out of bound exception
+
+2.second y is for you done editing and ready to execute
+	second n for you don't want to edit and continue normal execution
+
+*/
--- Experiment/csv/csvExp/CsvReader3.java
+++ Experiment/csv/csvExp/CsvReader3.java
@@ -3,7 +3,6 @@ package csv.csvExp;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class CsvReader3 {
