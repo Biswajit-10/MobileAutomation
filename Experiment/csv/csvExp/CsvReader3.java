e792ecc5ea86165d6ce432aa114437f878da347f
Biswajit-10 <88229173+Biswajit-10@users.noreply.github.com>
2023-04-26 12:35:37 +0530

    Update CsvReaderSuccess.java

--- Experiment/csv/csvExp/CsvReader3.java
+++ Experiment/csv/csvExp/CsvReader3.java
@@ -11,6 +11,44 @@ public class CsvReader3 {
 		csvReaderSuccess();
 		
 	}
+		
+	public static String[][] csvReaderSuccess() throws Exception {
+	String fileName = "data copy.csv";
+	List<List<String>> list = new ArrayList<List<String>>();
+	BufferedReader br = new BufferedReader(new FileReader(fileName));
+	String line = "";
+	while ((line = br.readLine()) != null) {
+		String[] headers = line.split(",",-1);
+		List<String> subList = new ArrayList<String>();
+//			for (String header : headers) {
+			for (int i = 0; i < headers.length; i++) {
+//				subList.add(header.trim());
+			subList.add(headers[i].trim());
+		}
+		list.add(subList);
+	}
+
+	System.out.println("List size is :" + list.size());
+
+	br.close();
+
+	int rows = list.size();
+	int cols = list.get(0).size();
+
+	String[][] array2D = new String[rows][cols];
+	for (int row = 0; row < rows; row++) {
+		for (int col = 0; col < cols; col++) {
+			try {
+				String string = list.get(row).get(col);
+				System.out.println("String is :"+string);
+				array2D[row][col] = list.get(row).get(col);
+			} catch (Exception e) {
+				System.out.println("row no and column no: "+row+","+col);
+			}
+		}
+	}
+	return array2D;
+	}
 	
 	public static String[][] csvReader(String csvFilePath) throws Exception {
 		
@@ -42,7 +80,7 @@ public class CsvReader3 {
 	}
 	
 	
-	public static void csvReaderSuccess() throws Exception {
+	public static void csvReaderSuccess1() throws Exception {
 
 		
 		String fileName = "./data/data.csv";
@@ -79,8 +117,6 @@ public class CsvReader3 {
 		System.out.println(array2D[1][1]);
 		System.out.println(array2D[2][1]);
 		System.out.println(array2D[3][1]);
-		
-	
 	}
 	
 }
